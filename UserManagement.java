cb951d331cd61e54c1f111befba2f22075cc30e5
NandunSamarasekara <nandunneelaka@gmail.com>
2025-02-23 23:11:17 +0800

    first commit

---
+++ UserManagement.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class UserManagement {
+    public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in);
+    
+    System.out.println("Car Service and Maintenance Tracker");
+    System.out.println("");
+    
+    
+    }
+}
