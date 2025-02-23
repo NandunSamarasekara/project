f2d6db20f1bf93849c9360410dea005f55080744
Nandun Samarasekara <nandunneelaka@gmail.com>
2025-02-23 23:34:28 +0800

    getting the details from the user

--- UserManagement.java
+++ UserManagement.java
@@ -1,11 +1,17 @@
 import java.util.Scanner;
 
 public class UserManagement {
+    private String firstname;
+    private String lastname;
+    private String password;
+    
     public static void main(String[] args){
     Scanner scanner = new Scanner(System.in);
     
     System.out.println("Car Service and Maintenance Tracker");
     System.out.println("");
+    
+    //about to be developed
     
     
     }
